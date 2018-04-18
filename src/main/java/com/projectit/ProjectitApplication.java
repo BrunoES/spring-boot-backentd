948f8ce2af0353c34b431131b0e0173ea515f165
Bruno Henrique <brunoh915@gmail.com>
2018-04-17 22:27:02 -0300

    Commit inicial

---
+++ src/main/java/com/projectit/ProjectitApplication.java
@@ -0,0 +1,12 @@
+package com.projectit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectitApplication.class, args);
+	}
+}
