b15bb3d392a09a2c608b853cbf8080b298a06271
Bruno Henrique <brunoh915@gmail.com>
2018-05-07 23:01:47 -0300

    Inclusao de operacao de criacao de categorias

--- src/main/java/com/projectit/ProjectitApplication.java
+++ src/main/java/com/projectit/ProjectitApplication.java
@@ -1,12 +1,34 @@
 package com.projectit;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjectitApplication {
+import com.projectit.domain.Categoria;
+import com.projectit.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjectitApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectitApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Tecnologia da Informação");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
