3cc2b959f34f79ff81e1e56cdf92ee0cbb78f864
Bruno Henrique <brunoh915@gmail.com>
2018-05-08 20:17:17 -0300

    Criacao de Classes Cidade e Estado e seus relacionamentos

--- src/main/java/com/projectit/ProjectitApplication.java
+++ src/main/java/com/projectit/ProjectitApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.projectit.domain.Categoria;
+import com.projectit.domain.Cidade;
+import com.projectit.domain.Estado;
 import com.projectit.domain.Produto;
 import com.projectit.repositories.CategoriaRepository;
+import com.projectit.repositories.CidadeRepository;
+import com.projectit.repositories.EstadoRepository;
 import com.projectit.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,10 @@ public class ProjectitApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectitApplication.class, args);
@@ -44,6 +52,19 @@ public class ProjectitApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
